be462e5d71a3d64d417038a37092e4f7072f74cb
konradgnat <konradmgnat@gmail.com>
2019-05-15 20:45:48 -0500

    progress

---
+++ testing/src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Hello there!');
+
+  console.log(div.innerHTML);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
